b18232c55b489101760f8dda5151b28cba5d8e4e
DavidPonce-dev <60764076+DavidPonce-dev@users.noreply.github.com>
2020-12-23 01:01:32 -0300

    Agregando componente Card

---
+++ src/components/Card.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="card" style="width: 18rem">
+    <img :src="img" class="card-img-top" alt="..." />
+    <div class="card-body">
+      <h5 class="card-title">{{titulo}}</h5>
+      <p class="card-text">{{texto}}</p>
+      <router-link
+        class="btn btn-primary"
+        :to="link"
+      >
+        {{linkText}}
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props:{
+    titulo:{
+      type:String,
+      required:false,
+      default:''
+    },
+    texto:{
+      type:String,
+      required:false,
+      default:''
+    },
+    img:{
+      type:Image,
+      required:false,
+      default:null
+    },
+    link:{
+      type:String,
+      required:false,
+      default:'#'
+    },
+    linkText:{
+      type:String,
+      required:false,
+      default:''
+    }
+  }
+};
+</script>
+
+<style>
+.card{
+  margin: 0 auto;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,50 @@
 <template>
+  <div class="container">
+    <div class="jumbotron">
+      <div class="row">
+        <Card
+        :img="require('../assets/vue-firebase.png')"
+        titulo= "Login vue"
+        class="col-6-sm"
+        texto= 'Este es un sistema de login basado en vue con firebase como backend. \\n aqui puedes iniciar sesion'
+        link= "/login"
+        linkText="IniciarSesion"
+      />
+        <Card
+        :img="require('../assets/vue-firebase.png')"
+        titulo= "Login vue"
+        class="col-6-sm"
+        texto= 'Este es un sistema de login basado en vue con firebase como backend. \n aqui puedes iniciar sesion'
+        link= "/login"
+        linkText="IniciarSesion"
+      />
+        <Card
+        :img="require('../assets/vue-firebase.png')"
+        titulo= "Login vue"
+        class="col-6-md"
+        texto= 'Este es un sistema de login basado en vue con firebase como backend. \n aqui puedes iniciar sesion'
+        link= "/login"
+        linkText="IniciarSesion"
+      />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import Card from '../components/Card.vue'
 export default {
-}
+  components: {
+    Card
+  }
+};
 </script>
+
+<style scoped>
+.container{
+  overflow-y: scroll;
+}
+.jumbotron {
+  background: transparent;
+}
+</style>

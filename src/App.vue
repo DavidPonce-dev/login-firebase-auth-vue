fd07aebd59960414709b91621c014d9c2f706e74
DavidPonce-dev <60764076+DavidPonce-dev@users.noreply.github.com>
2020-12-23 01:01:04 -0300

    Agregando estilos a la app

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,23 @@
 import Navbar from './components/Navbar'
 export default {
   components: {
-    Navbar
+    Navbar,
   },
 };
 </script>
+
+<style>
+body{
+  background: rgba(0, 0, 0, 0.5) url(./assets/wallpaper.jpg) no-repeat;
+  background-position: center;
+  background-size: cover;
+  min-height: calc(100vh - 57px);
+  min-width: 100%;
+  overflow: hidden;
+}
+@media (max-width: 576px) { 
+  body{
+    min-height: 100vh
+  }
+ }
+</style>
